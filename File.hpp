bd3cce09743d47d960f716da9aa2ab0fc3d99bbb
misanthropique <80142735+misanthropique@users.noreply.github.com>
2021-12-16 19:33:19 -0600

    Expanded the API

--- File.hpp
+++ File.hpp
@@ -4,7 +4,11 @@
  * or part by any means, without express prior written agreement is prohibited.
  */
 #pragma once
- 
+
+#include <atomic>
+#include <cstdint>
+#include <string>
+
 class File
 {
 private:
@@ -30,7 +34,9 @@ public:
 	File() noexcept;
 
 	/**
-	 * 
+	 * Constructor to open a file.
+	 * @param filepath Path to the file to be opened.
+	 * @param mode IOFlag of the file to be opened.
 	 */
 	File( const std::string& filepath, File::IOFlag mode );
 
@@ -64,9 +70,27 @@ public:
 	int64_t append( const uint8_t* buffer, uint32_t count );
 
 	/**
-	 * Close the file and release the resources if applicable.
+	 * Average of the observed read byte-rate (bytes per second).
+	 * @param ioFlag Flag indicating which byte-rate to return. If both Read and Write
+	 *               are set, then the Read byte-rate is returned. If neither Read nor
+	 *               Write are set, then NaN is returned. [default: File::IOFlag::Read]
+	 * @return An average of bytes read/written per second. NaN is returned on error,
+	 *         and zero indicates either nothing has been read/written, or you're using AvianIP (RFC-1149).
+	 */
+	double byteRate( File::IOFlag ioFlag = File::IOFlag::Read ) const;
+
+	/**
+	 * If applicable, close the file and release the resources.
+	 * Should no file be open, this method does nothing.
 	 */
 	void close();
+
+	/**
+	 * Get the current error message.
+	 * @param clearAfterRead Clear the error code after reading if set to true. [default: true]
+	 * @return A string containing the error message is returned.
+	 */
+	std::string errorMessage( bool clearAfterRead = true );
 
 	/**
 	 * Open a file to this File instance.
@@ -120,10 +144,61 @@ public:
 	int64_t read( uint8_t* buffer, uint32_t count );
 
 	/**
+	 * Reserve the requested number of bytes for the file size.
+	 * If the requested size is less than the current file size,
+	 * then this method does nothing. If the requested size is
+	 * greater than the current file size, then the file size will be
+	 * increased on disk to occupy the requested space. If there was
+	 * an issue allocating the requested space, then false is returned
+	 * and an error code will be assigned to the error code.
+	 * @param size The number of bytes requested that the file ought to occupy.
+	 * @param fill The byte value to fill in the new bytes.
+	 * @return True is returned if the requested number of bytes occupies
+	 *         the desired space on the drive. False is returned on error.
+	 */
+	bool reserve( int64_t size, uint8_t fill = '\0' );
+
+	/**
+	 * Resize the file to the desired number of bytes.
+	 * @param size The length of the file in bytes.
+	 * @param fill The byte value to fill in the new bytes.
+	 * @return True is returned if the requested number of bytes occupies
+	 *         the desired space on the drive. False is returned on error.
+	 */
+	bool resize( int64_t size, uint8_t fill = '\0' );
+
+	/**
+	 * Seek to the requested file offset.
+	 * @param offset Offset from either the beginning of the file or the current position.
+	 * @param relative When set to false, {@param offset} is from the beginning of the file
+	 *                 if positive, else from the end of the file if negative. When set to true,
+	 *                 {@param offset} is relative to the current file position. [default: false]
+	 * @return The difference between the requested file offset and the set file offset is returned.
+	 *         Negative one is returned on error and the error code is set.
+	 */
+	int64_t seek( int64_t offset, bool relative = false );
+
+	/**
 	 * Length of the file in bytes.
 	 * @return Length of the file in bytes. If the length is indeterminate, then -1 is returned.
 	 */
 	int64_t size() const;
+
+	/**
+	 * Synchronize the File instance with its source.
+	 * @return True is returned on success, else false is returned and the error code is set.
+	 */
+	bool sync();
+
+	/**
+	 * Truncate the file to the desired size. If {@param size} is less than
+	 * the current file size, then the file is truncated. If {@param size} is
+	 * greater than the current file size, then nothing is done.
+	 * @param size Size of the file in bytes to truncate the file to.
+	 * @return True is returned if the file has been truncated, false is returned
+	 *         if nothing was done, or an error has occurred.
+	 */
+	bool truncate( int64_t size );
 
 	/**
 	 * Write to the file the requested number of bytes and
