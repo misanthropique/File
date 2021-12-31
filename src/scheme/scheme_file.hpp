1517384b2b0e8929bd9d37508c00aeb7db3f796c
misanthropique <80142735+misanthropique@users.noreply.github.com>
2021-12-31 12:15:47 -0600

    Alphabetizing functions

--- src/scheme/scheme_file.hpp
+++ src/scheme/scheme_file.hpp
@@ -12,6 +12,13 @@
 #include "FileContext.hpp"
 #include "Scheme.hpp"
 
+// API Implementing Function Prototypes
+void __scheme_file_close(
+	struct FileContext* context );
+
+std::string __scheme_file_error_string(
+	struct FileContext* context );
+
 bool __scheme_file_open(
 	struct FileContext* context,
 	const std::string& uri,
@@ -24,17 +31,6 @@ int64_t __scheme_file_read(
 	uint32_t bytes,
 	bool updatePosition );
 
-int64_t __scheme_file_write(
-	struct FileContext* context,
-	const uint8_t* buffer,
-	uint32_t bytes,
-	bool append );
-
-int64_t __scheme_file_seek(
-	struct FileContext* context,
-	int64_t offset,
-	bool relative );
-
 bool __scheme_file_resize(
 	struct FileContext* context,
 	int64_t size,
@@ -42,19 +38,31 @@ bool __scheme_file_resize(
 	bool grown,
 	bool shrink );
 
-std::string __scheme_file_error_string(
+int64_t __scheme_file_seek(
+	struct FileContext* context,
+	int64_t offset,
+	bool relative );
+
+bool __scheme_file_sync(
 	struct FileContext* context );
 
-void __scheme_file_close(
-	struct FileContext* context );
+int64_t __scheme_file_write(
+	struct FileContext* context,
+	const uint8_t* buffer,
+	uint32_t bytes,
+	bool append );
+
+// Scheme API Constants
+const std::string SCHEME_FILE_CANONICAL_PREFIX( "file" );
 
 const struct SchemeAPI SCHEME_FILE_API =
 {
+	._F_close = __scheme_file_close,
+	._F_error_string = __scheme_file_error_string,
 	._F_open = __scheme_file_open,
 	._F_read = __scheme_file_read,
-	._F_write = __scheme_file_write,
-	._F_seek = __scheme_file_seek,
 	._F_resize = __scheme_file_resize,
-	._F_error_string = __scheme_file_error_string,
-	._F_close = __scheme_file_close
+	._F_seek = __scheme_file_seek,
+	._F_sync = __scheme_file_sync,
+	._F_write = __scheme_file_write
 };
